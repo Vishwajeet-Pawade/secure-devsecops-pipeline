f42a2d4876bcc5e3bd79019358b9bc62173b1f47
Vishwajeet-Pawade <vishwajeetpawade7@gmail.com>
2026-03-12 17:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import subprocess
 
 def run_command(user_input):
-    subprocess.call(user_input, shell=True)
+    subprocess.call(user_input)
 
 command = input("Enter command: ")
 run_command(command)
+
