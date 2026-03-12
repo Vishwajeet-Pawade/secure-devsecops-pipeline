0c4abd6d533c987085962264211fd7f671cb4db2
Vishwajeet-Pawade <vishwajeetpawade7@gmail.com>
2026-03-12 18:04:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import subprocess
 
 def run_command(user_input):
-    subprocess.call(user_input)
+    allowed_commands = ["date", "whoami", "pwd"]
+    
+    if user_input in allowed_commands:
+        subprocess.run([user_input], check=True)
+    else:
+        print("Command not allowed")
 
 command = input("Enter command: ")
 run_command(command)
-
